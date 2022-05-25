79f332e2f291267b318f32e5977a8918de37b750
vrkljam <vrkljam@earthlink.net>
2022-05-25 16:38:43 -0600

    add link sign in and create account

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Signin from './Routes/Signin';
 import Createaccnt from './Routes/Createaccnt';
+import{Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-    <Createaccnt />
-    {/* <Signin /> */}
 
+    <div>
+      <Routes>
+          <Route path='/createaccount' element={<Createaccnt />} />
+          <Route path='/signin' element={<Signin />} />
+          
+      </Routes>
+    </div>  
     </div>
   );
 }
