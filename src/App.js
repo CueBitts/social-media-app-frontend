af4be1bc94043aa639a4d280592d00875782592f
vrkljam <vrkljam@earthlink.net>
2022-05-25 12:45:39 -0600

    made-started create account page and sign in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Signin from './Routes/Signin';
+import Createaccnt from './Routes/Createaccnt';
 
 function App() {
   return (
     <div className="App">
-  
+    <Createaccnt />
+    {/* <Signin /> */}
+
     </div>
   );
 }
--- src/Routes/createAccnt.js
+++ src/Routes/createAccnt.js
@@ -1,13 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header'
+import '../index.css'
 
-function createAccnt() {
+function Createaccnt() {
+        const initialState ={
+              username:'', 
+              password:'',
+              passwordConfirm:'',
+              message:''}
+
+      const [formState, setFormState]=useState(initialState);
+
+      const handleChange = event =>{
+              setFormState({...formState, 
+                          [event.target.id]: 
+                          event.target.value});
+      }
+      const handleSubmit = event =>{
+              event.preventDefault();
+              setFormState(initialState)
+      }
+
+
   return (
-    <div>createAccnt
-        <Header />
+    <div>Create Account
+            <Header />
+            <form className='form' 
+            onSubmit={formState.password===formState.passwordConfirm 
+                    ? handleSubmit 
+                    : ""}>
+            
+            <label htmlFor="username">Username: </label>
+            <input 
+                id="username" 
+                type="text"
+                onChange={handleChange}
+                value={formState.username} />
+
+            <label htmlFor="password">Password:</label>
+            <input 
+                id="password" 
+                type="password"
+                onChange={handleChange}
+                value={formState.password} />
+                <br/>
+            
+            <label 
+            htmlFor="passwordConfirm">Confirm password: </label>
+            <input
+                type="password"
+                id="passwordConfirm"
+                onChange={handleChange}
+                value={formState.passwordConfirm}
+                />
+
+             <button type="submit">Create Account</button>   
+             <p>{formState.password===formState.passwordConfirm?"Confirmed" : "Retry"}</p>
+
+              <div>
+                <Link to ='/'> Home </Link>
+                
+              </div>
+        </form>
 
     </div>
   )
 }
 
-export default createAccnt
+export default Createaccnt
