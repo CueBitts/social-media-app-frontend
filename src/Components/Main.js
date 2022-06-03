7dc0e13c352340d3cd923c8c251bfb4a622f8acc
qhogan <gitASkl0921[1;6Dquinlanhogan@gmail.com>
2022-06-02 23:34:41 -0700

    route fixes 4

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -125,8 +125,9 @@ function Main() {
             <Sidebar />
             <BrowserRouter basename={process.env.PUBLIC_URL}>    
                 <Routes>
+                    <Route path='/' element={<Navigate to='/all' replace/>}/>
                     <Route 
-                        path='/' 
+                        path='/all' 
                         element={<Post
                             posts={posts} 
                             users={users}
